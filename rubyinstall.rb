2bdc8aa0b6a5fa600c818fe83bc60d0c39799241
jianbosun <wonderflow.sun@gmail.com>
2013-10-16 16:10:23 +0800

    find a bug about permmision

--- rubyinstall.rb
+++ rubyinstall.rb
@@ -11,9 +11,9 @@ def send_file(ssh,filename)
   end
 end
 
+#send huge blobs with a proceed tar
 def send_blobs(ssh,srcpath,despath)
   if File.exist?srcpath
-    ssh.exec!("mkdir ~/wonderflow/")
     ssh.scp.upload!( srcpath , despath , :recursive => true )do|ch, name, sent, total|
       print "\r#{name}: #{(sent.to_f * 100 / total.to_f).to_i}%"
     end
@@ -24,16 +24,24 @@ def send_blobs(ssh,srcpath,despath)
   end
 end
 
+#connect remote vm and run logical things
 def remote_connect(host,user,password)
   filename = File.join("log",host+".log")
   file = File.open(filename,"w")
   Net::SSH.start(host,user,:password=>password) do |ssh|
     puts host+" connected."
+
+    #send some files
     file.puts send_file(ssh,'sources.list')
     file.puts send_file(ssh,'rubyinstall.sh')
     file.puts send_file(ssh,'blobs/ruby-1.9.3-p448.tar.gz')
     file.puts send_file(ssh,'blobs/rubygems-1.8.17.tgz')
     file.puts send_file(ssh,'blobs/yaml-0.1.4.tar.gz')
+    #end
+
+    #send the important blob files
+    file.puts send_blobs(ssh,'adeploy.gz','.')
+    #end 
 
     ssh.open_channel do |channel|
       channel.request_pty do |ch,success|
@@ -41,7 +49,9 @@ def remote_connect(host,user,password)
         ch.exec('sudo bash rubyinstall.sh')
         ch.on_data do |ch,data|
           data.inspect
-          if data.inspect.include?"[sudo]"
+          if data.inspect.include?"[sudo]" 
+            channel.send_data("password\n")
+          elsif data.inspect.include?"Enter your password"
             channel.send_data("password\n")
           else
             file.puts data.strip
@@ -50,13 +60,15 @@ def remote_connect(host,user,password)
       end
     end
   end
+  file.close
 end 
 
 list = []
 #list << "10.10.102.154"
 #list << "10.10.102.155"
 #list << "10.10.102.156"
-list << "10.10.102.162"
+list << "10.10.102.170"
+list << "10.10.102.171"
 
 thread = []
 
--- rubyinstall.sh
+++ rubyinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# !!! something important about permission!!!
  
 if ! (which wget); then
   sudo apt-get update
@@ -8,13 +10,16 @@ fi
 if [ ! `which gcc` ]; then
   if [ `ls | grep sources.list` ]; then
     sudo cp sources.list /etc/apt/sources.list
+if [ ! `ls | grep ruby-1.9.3-p448.tar.gz` ]; then
+  sudo wget ${INSTALLER_URL}
+fi
     sudo apt-get update
     rm sources.list
   fi
   sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install gcc
 fi
 
-sudo apt-get -y install build-essential libssl-dev libreadline-gplv2-dev zlib1g-dev
+sudo apt-get -y install build-essential libssl-dev libreadline-gplv2-dev zlib1g-dev git-core libxslt-dev libxml2-dev
 
 sudo cp yaml-0.1.4.tar.gz /usr/src
 sudo cp ruby-1.9.3-p448.tar.gz /usr/src
@@ -23,7 +28,7 @@ sudo cp rubygems-1.8.17.tgz /usr/src
 
 #yaml install
 cd /usr/src
-sudo tar xvzf yaml-0.1.4.tar.gz
+sudo tar xzf yaml-0.1.4.tar.gz
 cd yaml-0.1.4
 sudo ./configure --prefix=/usr/local
 sudo make 
@@ -32,13 +37,10 @@ sudo make install
 #ruby install
 INSTALLER_URL=${INSTALLER_URL:-http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p448.tar.gz}
 source /etc/environment
-if [ ! `ls | grep ruby-1.9.3-p448.tar.gz` ]; then
-  sudo wget ${INSTALLER_URL}
-fi
 
 cd /usr/src
 if [ ! -d ruby-1.9.3-p448 ]; then
-  sudo tar xvzf ruby-1.9.3-p448.tar.gz
+  sudo tar xzf ruby-1.9.3-p448.tar.gz
 fi
 
 cd ruby-1.9.3-p448
@@ -50,6 +52,26 @@ fi
 
 #rubygems install
 cd /usr/src 
-sudo tar xvzf rubygems-1.8.17.tgz
+sudo tar xzf rubygems-1.8.17.tgz
 cd rubygems-1.8.17
 sudo ruby setup.rb
+
+#set blob files
+cd /home/vcap
+tar xzf adeploy.gz
+mkdir vcap
+mv  /home/vcap/adeploy/deploy /home/vcap/vcap/
+sudo mv  /home/vcap/adeploy/vcap /var
+
+
+#install gem packages
+sudo gem install bundler
+sudo gem install rake
+
+#get gem dependent files
+cd /home/vcap/vcap/deploy/nise_bosh/
+bundle install
+#ERROR,solution
+
+#install bosh
+sudo gem install bosh_cli
