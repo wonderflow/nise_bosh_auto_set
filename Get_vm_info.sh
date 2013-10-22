db73ed24c58804b9f66efb144a3f6628cd47cbfd
ketonfly <ketonfly@gmail.com>
2013-10-22 16:17:10 +0800

    Update Get_vm_info.sh
    for vm the "core" means processor

--- Get_vm_info.sh
+++ Get_vm_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #puts host 
-echo -n `cat /proc/cpuinfo | egrep "core id|physical id" | tr -d "\n" | sed s/physical/\\nphysical/g | grep -v ^$ | sort | uniq | wc -l` 
+echo -n `cat /proc/cpuinfo | grep "processor"|sort|uniq |wc -l` 
 echo -n ' ' 
 echo -n `cat /proc/meminfo | grep MemTotal |awk '{print $2}'`  
 echo -n ' ' 
