426653b081d22d017208b60abdac199471a24199
ketonfly <ketonfly@gmail.com>
2013-10-23 13:23:05 +0800

    Update Get_vm_info.sh
    ignore the unstable change line

--- Get_vm_info.sh
+++ Get_vm_info.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #puts host 
-echo -n `cat /proc/cpuinfo | grep "processor"|sort|uniq |wc -l` 
-echo -n ' ' 
-echo -n `cat /proc/meminfo | grep MemTotal |awk '{print $2}'`  
-echo -n ' ' 
+cat /proc/cpuinfo | grep "processor"|sort|uniq |wc -l
+cat /proc/meminfo | grep MemTotal |awk '{print $2}'
 df -h / | head -3 |tail -1 |awk '{print $1}'
