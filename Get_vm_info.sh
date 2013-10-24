79b697dc4fece0c4094dee68773d00519a92508c
ketonfly <ketonfly@gmail.com>
2013-10-24 13:49:27 +0800

    Update Get_vm_info.sh
    output in solid fomat

--- Get_vm_info.sh
+++ Get_vm_info.sh
@@ -2,4 +2,4 @@
 #puts host 
 cat /proc/cpuinfo | grep "processor"|sort|uniq |wc -l
 cat /proc/meminfo | grep MemTotal |awk '{print $2}'
-df -h / | head -3 |tail -1 |awk '{print $1}'
+df -Ph / | head -3 |tail -1 |awk '{print $1}'
